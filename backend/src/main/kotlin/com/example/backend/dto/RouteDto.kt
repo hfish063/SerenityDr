e882edd50d99baeecfccfdc00b6afc5c69ceb952
haydenfish <hayden.fish@icloud.com>
2025-02-26 15:35:52 -0800

    Add private access modifiers for class variables

--- backend/src/main/kotlin/com/example/backend/dto/RouteDto.kt
+++ backend/src/main/kotlin/com/example/backend/dto/RouteDto.kt
@@ -3,9 +3,9 @@ package com.example.backend.dto
 import com.example.backend.entity.Route
 
 data class RouteDto(
-        val title: String,
-        val description: String?,
-        val uid: String
+        private val title: String,
+        private val description: String?,
+        private val uid: String
 ) : BaseDto<Route> {
     override fun toEntity(): Route {
         return Route(title = this.title, description = this.description, uid = this.uid)
