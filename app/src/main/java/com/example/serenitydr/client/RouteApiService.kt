3379edb3726242c8f72bd01eff1cf05ea056c04d
haydenfish <hayden.fish@icloud.com>
2025-02-28 13:32:10 -0800

    Add Route insert operation

--- app/src/main/java/com/example/serenitydr/client/RouteApiService.kt
+++ app/src/main/java/com/example/serenitydr/client/RouteApiService.kt
@@ -1,8 +1,12 @@
 package com.example.serenitydr.client
 
 import com.example.serenitydr.model.RouteModel
+import com.example.serenitydr.request.RouteRequest
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
 
 val routeApiService: RouteApiService =
     ApiClient.buildService(serviceClass = RouteApiService::class.java)
@@ -10,4 +14,8 @@ val routeApiService: RouteApiService =
 interface RouteApiService {
     @GET("/routes/all")
     fun getAllRoutes(): Response<List<RouteModel>>
+
+    @Headers("Content-Type: application/json")
+    @POST("/routes/save")
+    fun saveRoute(@Body routeRequest: RouteRequest): Response<RouteModel>
 }
