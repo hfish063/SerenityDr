191e0699a59d3372339ec513320484cc90435dba
haydenfish <hayden.fish@icloud.com>
2025-02-28 13:26:27 -0800

    Refactor ApiClient object

--- app/src/main/java/com/example/serenitydr/client/RouteApiService.kt
+++ app/src/main/java/com/example/serenitydr/client/RouteApiService.kt
@@ -4,6 +4,9 @@ import com.example.serenitydr.model.RouteModel
 import retrofit2.Response
 import retrofit2.http.GET
 
+val routeApiService: RouteApiService =
+    ApiClient.buildService(serviceClass = RouteApiService::class.java)
+
 interface RouteApiService {
     @GET("/routes/all")
     fun getAllRoutes(): Response<List<RouteModel>>
