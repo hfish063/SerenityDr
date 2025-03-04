5f2e65b5e54437fdc37ac60e949e444055d148d9
haydenfish <hayden.fish@icloud.com>
2025-03-03 19:52:46 -0800

    Setup ViewModel for ViewRouteScreen

--- app/src/main/java/com/example/serenitydr/ui/screens/viewRouteScreen/ViewRouteScreenViewModel.kt
+++ app/src/main/java/com/example/serenitydr/ui/screens/viewRouteScreen/ViewRouteScreenViewModel.kt
@@ -1,6 +1,25 @@
 package com.example.serenitydr.ui.screens.viewRouteScreen
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.serenitydr.model.Route
+
+data class ViewRouteScreenState(
+    val isLoading: Boolean = true,
+    val route: Route? = null,
+    val error: String? = null
+)
 
 class ViewRouteScreenViewModel : ViewModel() {
+    private val _routeState = mutableStateOf(ViewRouteScreenState())
+    val routeState: State<ViewRouteScreenState> = _routeState
+
+    init {
+        fetchRoute()
+    }
+
+    private fun fetchRoute() {
+        // TODO: Fetch route by id from ApiService class method
+    }
 }
