94bfe8da0f28587d1a0c0377c40d05cd67989a57
haydenfish <hayden.fish@icloud.com>
2025-03-05 12:22:25 -0800

    Create LoadingIcon composable

---
+++ app/src/main/java/com/example/serenitydr/ui/composables/LoadingIcon.kt
@@ -0,0 +1,20 @@
+package com.example.serenitydr.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoadingIcon() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(modifier = Modifier)
+    }
+}
