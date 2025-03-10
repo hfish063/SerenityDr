1cd726062f87d222b7714d34d0b19b6984175d2e
GeoAce16 <GeorgeAcevedo17@gmail.com>
2025-03-10 14:10:48 -0700

    Added NavItem as well

--- app/src/main/java/com/example/serenitydr/ui/screens/uiScreen/NavItem.kt
+++ app/src/main/java/com/example/serenitydr/ui/screens/uiScreen/NavItem.kt
@@ -1,7 +1,7 @@
 package com.example.serenitydr.ui.screens.uiScreen
 
 import androidx.compose.ui.graphics.vector.ImageVector
-
+//comment
 data class NavItem(
     val label: String,
     val icon: ImageVector,
